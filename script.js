f1e2bdb65437f1a5e2d642f3d805c422be2a0c4c
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-04-22 23:05:10 +0500

    testing 3

--- script.js
+++ script.js
@@ -31,63 +31,63 @@ document.getElementById("check").addEventListener("click", function (event) {
   document.getElementById("output").innerHTML = "";
   let cityName = document.getElementById("cityName").value
 
-  // List of valid city names in Pakistan
-  // const validCities = [
-  //   "karachi",
-  //   "lahore",
-  //   "islamabad",
-  //   "faisalabad",
-  //   "rawalpindi",
-  //   "multan",
-  //   "gujranwala",
-  //   "peshawar",
-  //   "quetta",
-  //   "sargodha",
-  //   "sialkot",
-  //   "bahawalpur",
-  //   "sukkur",
-  //   "jhang",
-  //   "sheikhupura",
-  //   "gujrat",
-  //   "mardan",
-  //   "larkana",
-  //   "rahim yar khan",
-  //   "kasur",
-  //   "rahim yar khan",
-  //   "sahiwal",
-  //   "okara",
-  //   "wah cantonment",
-  //   "dera ghazi khan",
-  //   "mirpur khas",
-  //   "jhelum",
-  //   "sanghar",
-  //   "mandi bahauddin",
-  //   "hafizabad",
-  //   "jacobabad",
-  //   "kohat",
-  //   "shikarpur",
-  //   "muzaffargarh",
-  //   "khanewal",
-  //   "hassan abdal",
-  //   "dera ismail khan",
-  //   "chiniot",
-  //   "sukkur",
-  //   "atd",
-  //   "layyah",
-  //   "swabi",
-  //   "khushab",
-  //   "abad",
-  //   "lala musa",
-  //   "vehari",
-  //   "hyderabad"
-  // ];
+  //List of valid city names in Pakistan
+  const validCities = [
+    "karachi",
+    "lahore",
+    "islamabad",
+    "faisalabad",
+    "rawalpindi",
+    "multan",
+    "gujranwala",
+    "peshawar",
+    "quetta",
+    "sargodha",
+    "sialkot",
+    "bahawalpur",
+    "sukkur",
+    "jhang",
+    "sheikhupura",
+    "gujrat",
+    "mardan",
+    "larkana",
+    "rahim yar khan",
+    "kasur",
+    "rahim yar khan",
+    "sahiwal",
+    "okara",
+    "wah cantonment",
+    "dera ghazi khan",
+    "mirpur khas",
+    "jhelum",
+    "sanghar",
+    "mandi bahauddin",
+    "hafizabad",
+    "jacobabad",
+    "kohat",
+    "shikarpur",
+    "muzaffargarh",
+    "khanewal",
+    "hassan abdal",
+    "dera ismail khan",
+    "chiniot",
+    "sukkur",
+    "atd",
+    "layyah",
+    "swabi",
+    "khushab",
+    "abad",
+    "lala musa",
+    "vehari",
+    "hyderabad"
+  ];
 
-  // // Check if the entered city name exists in the list of valid cities
-  // if (!validCities.includes(cityName.toLowerCase())) {
-  //   document.getElementById("output").innerHTML =
-  //     "Please enter a valid city name of Pakistan.";
-  //   return;
-  // }
+  // Check if the entered city name exists in the list of valid cities
+  if (!validCities.includes(cityName.toLowerCase())) {
+    document.getElementById("output").innerHTML =
+      "Please enter a valid city name of Pakistan.";
+    return;
+  }
 
   let date = new Date().toISOString().slice(0, 10);
 
